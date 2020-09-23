2077bc638eb0e0a6ca0b836af8525c4218761000
Andres Fernando <andres.fernando08@gmail.com>
2020-09-22 22:42:58 -0500

    [add] linked endpoints and database connection to the entrypoint

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ const environment = process.env.NODE_ENV
 const port        = process.env.APP_PORT
 
 // set routes
-
+const routes    = require('./routes/index');
 
 // set events
 
 
 // connect the database
-
+require('./database');
 
 // set middlewares
 app.use(morgan(environment == 'local' ? 'dev' : 'combined'));
@@ -26,11 +26,7 @@ app.use(express.json());
 
 
 // initialize routes
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'API Rest'
-    })
-})
+app.use('/', routes);
 
 // init port
 if (!port) {
