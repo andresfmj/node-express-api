3261eb0988ddb4184e03fc0d7ea31bf66c9d2eb7
Andres Fernando <andres.fernando08@gmail.com>
2020-09-23 21:15:58 -0500

    [add] implemented friends and random hunters endpoints

--- routes/hunters.js
+++ routes/hunters.js
@@ -6,37 +6,103 @@ const mongoose = require('mongoose');
 // const UserModel     = require('../models/user');
 const HunterModel = require('../models/hunter');
 
-router.post('/', async (req, res) => {
+router.get('/friends', async (req, res) => {
 	const { user, hunterSelected } = req.body;
 
-	if (!user || !user.friends || !hunterSelected) {
+	if (!user || !hunterSelected) {
 		return res.status(406).json({
 			error: true,
 			message: 'user, hunterSelected are required parameters',
 		});
-    }
-    
-    const friends = user.friends.map(i => {
-        return mongoose.Types.ObjectId(i)
-    })
+	}
+	
+	if (!user.friends || user.friends.length == 0) {
+		return res.status(406).json({
+			error: true,
+			message: 'user does not have any friends'
+		})
+	}
+	
+	const hunterSel = await HunterModel.findOne({ _id: mongoose.Types.ObjectId(hunterSelected) })
 
-	const hunters = await HunterModel.find({
-		user: {
-			$in: friends,
-		},
-		locked: false,
-    }).limit(10);
-    
-    console.log(hunters)
+	if (hunterSel) {
+		// debido a que el hunterSelected especificado en el documento User,
+		// desafortunadamente no encontrará ningun cercano a su level
+		const levelSup = hunterSel.level + 10
+		const levelInf = hunterSel.level - 10
 
+		const query = {
+			"$and": [ 
+				{
+					"user": {
+						"$in": user.friends.map(i => mongoose.Types.ObjectId(i))
+					}
+				},
+				{ "level": {$gte: levelInf} }, { "level": {$lte: levelSup} }, 
+				{ "locked": false } 
+			]
+		}
+
+		const hunters = await HunterModel.find(query)
+
+		res.status(200).json({
+			error: hunters.length > 0 ? false : true,
+			message: '',
+			rows: hunters.length,
+			results: hunters
+		})
+
+	} else {
+		res.status(404).json({
+			error: true,
+			message: 'Hunter selected not found'
+		})
+	}
 
 });
 
 
-router.post('/random', async (req, res) => {
-    const { user } = req.body
+router.get('/random', async (req, res) => {
+    const { user, hunterSelected } = req.body;
 
-    
+    if (!hunterSelected) {
+		return res.status(406).json({
+			error: true,
+			message: 'hunterSelected is a required parameter',
+		});
+	}
+	
+	const hunterSel = await HunterModel.findOne({ _id: mongoose.Types.ObjectId(hunterSelected) })
+
+	if (hunterSel) {
+		const levelSup = hunterSel.level + 10
+		const levelInf = hunterSel.level - 10
+
+		const hunters = await HunterModel.aggregate([
+			{ 
+				$match: { 
+					$and: [ 
+						{ level: {$gte: levelInf} }, { level: {$lte: levelSup} }, 
+						{ _id: {$ne: hunterSel._id} }, 
+						{ locked: false } 
+					] 
+				} 
+			},
+			{ $sample: { size: 10 } }
+		])
+
+		res.status(200).json({
+			error: false,
+			message: '',
+			results: hunters
+		})
+
+	} else {
+		res.status(404).json({
+			error: true,
+			message: 'Hunter not found'
+		})
+	}
 
 })
 
